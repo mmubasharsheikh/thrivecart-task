af7d989b207b94dbf6cf308c909eed7487da5448
Mubashar Jamshad <m.mubasharsheikh@gmail.com>
2025-04-03 18:43:20 +0500

    Defined product catalogue and delivery rules

--- app/Basket.php
+++ app/Basket.php
@@ -1,4 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 class Basket {
+    public function __construct(
+        private readonly array $catalogue,
+        private readonly array $deliveryRules,
+        private readonly array $offers = [],
+        private array $items = [],
+    ) {}
 }
